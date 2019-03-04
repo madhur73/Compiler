0df40e357d86738db665d03b28cc89286787fcc1
codingisbliss <42849460+codingisbliss@users.noreply.github.com>
2019-03-04 17:46:33 -0500

    Update Scanner.py
    1.0.1

--- Scanner.py
+++ Scanner.py
@@ -3,78 +3,45 @@ from token import token as tk
 
 symTbl = {}
 keyword ={"array":"KW_ARRAY" , 
-		"tuple":"KW_TUPLE",
-		"local":"KW_LOCAL" ,
-		"global":"KW_GLOBAL",
-		"defun":"KW_DEFUN","end":"KW_END",
-		"while":"KW_WHILE",
-		"do":"KW_DO" ,
-		"if":"KW_IF" ,
-		"then":"KW_THEN" ,
-		"elsif":"KW_ELSIF" ,
-		"else":"KW_ELSE",
-		"foreach":"KW_FOREACH",
-		"in":"KW_IN",}
+	"tuple":"KW_TUPLE",
+	"local":"KW_LOCAL" ,
+	"global":"KW_GLOBAL",
+	"defun":"KW_DEFUN","end":"KW_END",
+	"while":"KW_WHILE",
+	"do":"KW_DO" ,
+	"if":"KW_IF" ,
+	"then":"KW_THEN" ,
+	"elsif":"KW_ELSIF" ,
+	"else":"KW_ELSE",
+	"foreach":"KW_FOREACH",
+	"in":"KW_IN",}
 char = {"[":"LBRAK",
-			"]":"RBRAK" ,
-			 ";":"SEMI",
-			 "(":"LPAR" ,
-			 ")":"RPAR"
-			}
-operators = {"..":"OP_DOTDOT",
-		",":"OP_COMMA",
-		".":"OP_DOT",
-		"<":"OP_LESS" ,
-		">":"OP_GREATER" ,
-		"<=":"OP_LESSEQUAL" ,
-		 ">=":"OP_GREATEREQUAL",
-		"==":"OP_EQUAL",
-		"!=":"OP_NOTEQUA",
-		"+":"OP_PLUS",
-		 "-":"OP_MINUS",
-		 "*":"OP_MULT",
-		 "/":"OP_DIV",
-		 "-":"OP_UMINUS",
-		 ",":"OP_COMMA",
-		 "=":"ASSIGN"
+	"]":"RBRAK" ,
+	 ";":"SEMI",
+	 "(":"LPAR" ,
+	 ")":"RPAR"
+	}
+operators = {
+	"..":"OP_DOTDOT",
+	",":"OP_COMMA",
+	".":"OP_DOT",
+	"<":"OP_LESS" ,
+	">":"OP_GREATER" ,
+	"<=":"OP_LESSEQUAL" ,
+	 ">=":"OP_GREATEREQUAL",
+	"==":"OP_EQUAL",
+	"!=":"OP_NOTEQUA",
+	"+":"OP_PLUS",
+	 "-":"OP_MINUS",
+	 "*":"OP_MULT",
+	 "/":"OP_DIV",
+	 "-":"OP_UMINUS",
+	 ",":"OP_COMMA",
+	 "=":"ASSIGN"
 		}
 
 
 class Scanner:
-
-	#WORK TO BE DONE
-	#scan this tokens
-	""""RETURN: "return"
-	PRINT: "print"
-	ASSIGN: "="
-	EXCHANGE: "<->"
-	"""
-	#[BUG] *** CAN BE TAKEN AS *
-	#[SOLVED]alpha numeric are cuasing some ERRor
-	#[BUG] = SIGN CREATING PROBLEM
-	#[BUG]handle +++ or +<space>
-	#[UNSOLVED]unary vs minus Operator fix
-	#[SOLVED]symbol tAble class inheritance
-	#[NOIDEA]I do not want to assume a particular end-of-line convention (CR on old Macs, NL on Unix/Linux/Mac OS, CR LN on Windows)----.  
-	#Would be best to write code that does not care.
-	#[SOLVED]you have to put a limit on the length of IDs (no arbitrarily long IDs).-----  
-	#you limit should not be more than 80.  ifthe scanner sees an ID of length more tha the limit, it should process it, 
-	#complain about it in a ERROR message, and then truncate it to the limit.
-	#[NOTKNOWN]formfeeds 
-	#f[SOLVED]or the purposes of this part of the project, COMMENT is a token.  They should show up in the test output----
-	#  But build in a switch to turn it off, so that they are just silently ignored, for later parts of the project.
-	# [UNSOLVED]be able to read a file for input, or standard input
-	#[UNSOLVED]each time there is a problem, it has to output the line number, character positions (start and finish) of the problem
-	#[UNSOLVED]Support the operation next()
-	#[NOTSOLVED]next() operation to advnace the scanner.  if scanner sees errors, it complains about them to standard output, skips them, and keeps going.  
-	#it does NOT report them to the caller (we will discuss the advantages and disadvantages of this in class)
-	#[UNSOLVED] add error msg to symbol table:
-
-
-	#FUTURE WORK
-	#unput() or backup()
-	#eading file should be efficient while using temporary buffer
-	
 	def __init__(self):
 		self.linecnt = 0
 		self.charpos = 0
@@ -110,13 +77,8 @@ class Scanner:
 				self.charpos+=1
 				self.forwd+=1
 				if(self.buffer[self.forwd] == "\n"):
-					
 					self.linecnt+=1
-
 				break
-	#print all stats
-	def printall(self):
-		print("linecnt",self.linecnt,"charpos",self.charpos,"forwd",self.forwd,"follow",self.follow)
 
 	#return next character
 	def peek(self):
@@ -141,13 +103,9 @@ class Scanner:
 		if(self.isKeyword(word)):
 			self.addtoTable(keyword[word
 				],word,self.linecnt,self.follow,self.forwd)
-			#print("Keyword ",word)
-			#self.addtoTable("ID"+str(self.idCount),temp,self.linecnt,self.follow,self.forwd)
 		elif(word == "print"):
-			#print("got print",word)
 			self.addtoTable("PRINT",word,self.linecnt,self.follow,self.forwd)
 		elif(word == "return"):
-			#print("got RETURN")
 			self.addtoTable("RETURN",word,self.linecnt,self.follow,self.forwd)
 		elif(self.isId(word)):
 			if(len(word)>= 80):
@@ -159,14 +117,12 @@ class Scanner:
 				self.addtoTable("ID"+str(self.idCount),word2,self.linecnt,self.follow,self.forwd)
 				self.idCount+=1
 			else:
-
 				self.addtoTable("ID"+str(self.idCount),word,self.linecnt,self.follow,self.forwd)
 				self.idCount+=1
-	#getnumber algorithm
+	#get number algorithm
 	def getNum(self):
 		v = 0
-		while True:
-			
+		while True:		
 			if(self.isDigit(self.peek())):
 				v = v*10 + int(self.buffer[self.forwd])
 				self.forwd+=1
@@ -174,32 +130,26 @@ class Scanner:
 				v = v*10 + int(self.buffer[self.forwd])
 				if(v> 2147483648 or v < -2147483648 or len(str(v))>10):
 					print("WARNING: Size of Integer above limit. ")
-
-				#print(self.buffer[self.follow:self.forwd+1])
-				#print("ID_INT",v)
 				self.addtoTable("ID_INT",v,self.linecnt,self.follow,self.forwd)
 				break
 	def getOp(self):
+		#peek next chracter from buffer if it matches in operator table return else if it does not matches
+		#return single operaor else and error
 		temp = self.buffer[self.forwd]
 		ch = temp[0]
 		temp = (self.peek())
 		ch+=temp[0]
 		if(self.isOp(ch)):
-			#print("got double op",ch)
-			#if(self.isOp(self.buffer[self.forwd])):
-				#print("got single op",self.buffer[self.forwd])
 			self.addtoTable(operators[ch],ch,self.linecnt,self.follow,self.forwd)
-		
+		#Checking for comment ***[THis section is stored in symbol table]
 		elif(ch == "**"):
 			if(self.isComment()):
-				#print("Comment starts")
 				self.skipComment()
 		elif(ch =="<-"):
 			if(self.buffer[self.forwd+2] == ">"):
 				#print("got exchange")
 				self.addtoTable("EXCHANGE",ch+">",self.linecnt,self.follow,self.forwd)
 				self.forwd+=2
-
 		else:
 			#nknown operators
 			#error for u
@@ -208,29 +158,21 @@ class Scanner:
 	def skipComment(self):
 		temp = ""
 		while(self.buffer[self.forwd]!= "\n"):
-			#print(self.buffer[self.forwd])
 			if(self.peek()== 'eof'):
 				break
 			temp+=self.getCurr()
 			self.forwd+=1
-			#if(self.getCurr() == "\n"):
-			#	self.linecnt+=1
-
 		self.addtoTable("COMMENT",temp[3:],self.linecnt,self.follow+3,self.forwd)
 	def isKeyword(self,ch):
-		if(ch in keyword):
-			
+		if(ch in keyword):	
 			return True
 		else:
 			return False
 	def isOp(self,ch):
-		
 		if(ch in operators):
-			
 			return True
 		else:
 			return (False)
-	
 	#check for character if Digit
 	def isDigit(self,ch):
 		digit = re.compile("[0-9]")
@@ -246,6 +188,7 @@ class Scanner:
 			return False
 		else:
 			return True
+	#check If its an ID or not by scanning for alphabet or '_'
 	def isId(self,ch):
 		count =0 
 		for c in  ch:
@@ -256,7 +199,6 @@ class Scanner:
 				if(ascii== 95 or(ascii<=90 and ascii >=65) or(ascii >= 97 and ascii<=122)):
 					count +=1
 		if(len(ch) == count):
-			#print("ID",ch)
 			return True
 		else:
 			return	False
@@ -265,8 +207,6 @@ class Scanner:
 			return True
 		else:
 			return False
-
-
 	def isWhite(self):
 		if(self.peek() == " " or self.peek() == "\n" or self.peek() == "\t"  ):
 			return True
@@ -278,7 +218,6 @@ class Scanner:
 			return True
 		else:
 			return False
-
 	def getCurr(self):
 		return self.buffer[self.forwd]
 
@@ -293,8 +232,6 @@ class Scanner:
 			if(self.isLetter(self.buffer[self.forwd])):
 				self.follow = self.forwd
 				if(self.isLetter(self.peek())):
-					#self.forwd+=1
-					#self.charpos+=1
 					self.getword()
 				elif(self.isWhite()):
 					self.getword()
@@ -316,19 +253,17 @@ class Scanner:
 					self.getOp()
 			elif(self.isotherChar(self.getCurr())):
 				self.follow =self.forwd
-
 			elif(self.getCurr() == "\n"):
 				
 				self.linecnt+=1
 				self.linecol.append((self.linecnt,self.forwd))
-				#print(self.linecol)
 			else:
 				print("illegal charcter used:",self.getCurr())
 
 
 			self.forwd+=1
 #implementing symbol table as Hash Table
-	#get colmn number from linecol[linenum][0] - startops
+#get colmn number from linecol[linenum][0] - startops
 
 
 print("id\ttype\tlinenum\tstartpos\tendpos\terromsg\t(line,col)")
