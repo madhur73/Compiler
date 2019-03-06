2e06b1e429f9dfa2677bf6c17521720d09cca59a
Max Marrone <max@marrone.nyc>
2019-03-05 21:56:01 -0500

    Replace scanner.py with scanner2.py.

--- Max/scanner.py
+++ Max/scanner.py
@@ -3,11 +3,92 @@ import string
     
 from token import *
 
+# Each token type corresponds to a regular expression pattern.  Each pattern is
+# wrapped as a group, and those groups are concatenated into one big mega-regex.
+# The scanner attempts to match this mega-regex against the remaining input. The
+# group that sucessfully matches, if there is one, corresponds to the next
+# token.  Ordering is significant: longer operators like >= should appear before
+# shorter operators like >, so they take precedence.
+_patterns = [
+    (TokenType.COMMENT,         r"\*\*\*.*"),
+    (TokenType.OP_COMMA,        r","),
+    (TokenType.SEMI,            r";"),
+    (TokenType.LPAR,            r"\("),
+    (TokenType.RPAR,            r"\)"),
+    (TokenType.LBRAK,           r"\["),
+    (TokenType.RBRAK,           r"\]"),
+    (TokenType.OP_DOTDOT,       r"\.\."),
+    (TokenType.OP_DOT,          r"\."),
+    (TokenType.INT_LIT,         r"[0-9]+"),
+    (TokenType.ID,              r"[a-zA-Z_]+"),
+    (TokenType.EXCHANGE,        r"<->"),
+    (TokenType.OP_LESSEQUAL,    r"<="),
+    (TokenType.OP_GREATEREQUAL, r">="),
+    (TokenType.OP_LESS,         r"<"),
+    (TokenType.OP_GREATER,      r">"),
+    (TokenType.OP_EQUAL,        r"=="),
+    (TokenType.OP_NOTEQUA,      r"!="),
+    (TokenType.ASSIGN,          r"="),
+    (TokenType.OP_PLUS,         r"\+"),
+    (TokenType.OP_MINUS,        r"-"),
+    (TokenType.OP_MULT,         r"\*"),
+    (TokenType.OP_DIV,          r"/"),
+]
+
+# Keywords are just IDs with special values.  They can't be directly included
+# in the mega-regex because they can interfere with scanning IDs.
+_keywords = {
+    "array":   TokenType.KW_ARRAY,
+    "defun":   TokenType.KW_DEFUN,
+    "do":      TokenType.KW_DO,
+    "else":    TokenType.KW_ELSE,
+    "elsif":   TokenType.KW_ELSIF,
+    "end":     TokenType.KW_END,
+    "foreach": TokenType.KW_FOREACH,
+    "global":  TokenType.KW_GLOBAL,
+    "if":      TokenType.KW_IF,
+    "in":      TokenType.KW_IN,
+    "local":   TokenType.KW_LOCAL,
+    "print":   TokenType.PRINT
+    "return":  TokenType.RETURN,
+    "then":    TokenType.KW_THEN,
+    "tuple":   TokenType.KW_TUPLE,
+    "while":   TokenType.KW_WHILE,
+}
+
+_mega_regex = re.compile("|".join("("+p+")" for t, p in _patterns))
+
+# Returns the token at the start of source[begin:] as a tuple (type, value),
+# or (None, "") if there is no valid token there.
+def _next_token(source, begin):
+    # Technique inspired by an example from the Python regex docs:
+    # https://docs.python.org/3/library/re.html#writing-a-tokenizer
+    match = _mega_regex.match(source, begin)
+    if match:
+        matched_group_index = match.lastindex - 1
+        token_type, _ = _patterns[matched_group_index]
+        value = match[0]
+        if token_type == TokenType.ID:
+            token_type = _keywords.get(value, TokenType.ID)
+        return token_type, value
+    else:
+        return None, ""
+
 class Scanner:
-    def __init__(self, *, string="", filepath=None, emit_comments=True):
+    def __init__(self, *, string="", filepath=None, file=None, emit_comments=True):
+        """Initializes a Scanner.
+        
+        The full source must be provided as either a single string, a file, or a path to a file.
+        If a file is provided, it will not be closed.
+        
+        Comment tokens will be included in the output stream only if emit_comments is true.
+        """
+        
         if filepath:
             with open(filepath) as file:
                 string = file.read()
+        else if file:
+            string = file.read()
         
         self._s = string
         self._emit_comments = emit_comments
@@ -20,13 +101,16 @@ class Scanner:
         self._line = 1
         self._column = 1
     
-    def _warn(self, message):
-        print(f"{self._line}:{self._column} Warning: {message}")
+    # Print a warning with a position tag from the current position with a length of error_length.
+    def _warn(self, message, error_length):
+        print(f"{self._line}:{self._column}-{self._line}:{self._column+error_length} Warning: {message}")
     
     def _advance_b(self, amount):
         self._b += amount
         self._column += amount
     
+    # Skip over whitespace, including newlines, and advance the file offset,
+    # line counter and column counter as necessary.
     def _skip_whitespace(self):
         while self._b < len(self._s) and self._s[self._b] in string.whitespace:
             if self._s[self._b] == "\n":
@@ -34,108 +118,41 @@ class Scanner:
                 self._column = 0 # Will be advanced to 1 below.
             self._advance_b(1)
     
-    def _scan_comment(self):
-        match = re.match(r"\*\*\*.*", self._s[self._b:]) # Note the . will not match newlines.
-        if match:
-            return TokenType.COMMENT, match[0], len(match[0])
-        else:
-            return None
-    
-    def _scan_operator(self):
-        T = TokenType
-        operators = [ # Order matters, e.g. OP_DOTDOT is preferred over two OP_DOTs.
-            ("..",  T.OP_DOTDOT),
-            (".",   T.OP_DOT),
-            ("[",   T.LBRAK),
-            ("]",   T.RBRAK),
-            ("(",   T.LPAR),
-            (")",   T.RPAR),
-            (";",   T.SEMI),
-            (",",   T.OP_COMMA),
-            ("==",  T.OP_EQUAL),
-            ("!=",  T.OP_NOTEQUA),
-            ("=",   T.ASSIGN),
-            ("<->", T.EXCHANGE),
-            ("<=",  T.OP_LESSEQUAL),
-            ("<",   T.OP_LESS),
-            (">=",  T.OP_GREATEREQUAL),
-            (">",   T.OP_GREATER),
-            ("+",   T.OP_PLUS),
-            ("-",   T.OP_MINUS),
-            ("*",   T.OP_MULT),
-            ("/",   T.OP_DIV)
-        ]    
-        for string, token_type in operators:
-            if self._s.startswith(string, self._b):
-                return token_type, string, len(string)
-        return None
-    
-    def _scan_id_or_keyword(self):
-        keywords = {
-            "array":   TokenType.KW_ARRAY,
-            "tuple":   TokenType.KW_TUPLE,
-            "local":   TokenType.KW_LOCAL,
-            "global":  TokenType.KW_GLOBAL,
-            "defun":   TokenType.KW_DEFUN,
-            "end":     TokenType.KW_END,
-            "while":   TokenType.KW_WHILE,
-            "do":      TokenType.KW_DO,
-            "if":      TokenType.KW_IF,
-            "then":    TokenType.KW_THEN,
-            "elsif":   TokenType.KW_ELSIF,
-            "else":    TokenType.KW_ELSE,
-            "foreach": TokenType.KW_FOREACH,
-            "in":      TokenType.KW_IN,
-            "return":  TokenType.RETURN,
-            "print":   TokenType.PRINT
-        }
-        match = re.match("[a-zA-Z_]+", self._s[self._b:])
-        if match:
-            string = match[0]
-            token_type = keywords.get(string, TokenType.ID)
-            if len(string) > 80:
-                self._warn(f'identifier "{string}" will be truncated to 80 characters.')
-            return token_type, string[:80], len(string)
-        return None
-    
-    def _scan_integer_literal(self):
-        match = re.match("[0-9]+", self._s[self._b:])
-        if match:
-            string = match[0]
-            if int(string) >= 2**31:
-                self._warn(f'integer "{string}" will be clamped to 2^31.')
-                string = str(2**31)
-            return TokenType.INT_LIT, string, len(match[0])
-        return None
-    
     def next(self):
+        """Consumes and returns the next token, or an EOF token if there are none left."""
+        
         while True:
             self._skip_whitespace()
             
             if self._b >= len(self._s):
                 return Token(TokenType.EOF, "", self._b, self._b, self._line, self._column)
             
-            scan_tasks = [
-                self._scan_comment,
-                self._scan_operator,
-                self._scan_id_or_keyword,
-                self._scan_integer_literal
-            ]
+            token_type, raw_value = _next_token(self._s, self._b)
             
-            for scan_task in scan_tasks:
-                scan_result = scan_task()
-                if not scan_result: continue
-                token_type, token_string, advancement = scan_result
-                if (not self._emit_comments) and (token_type == TokenType.COMMENT):
-                    continue
-                token = Token(token_type, token_string, self._b, self._b+advancement, self._line, self._column)
-                self._advance_b(advancement)
+            if token_type is None:
+                self._warn(f'unrecognized character "{self._s[self._b]}".', 1)
+                self._advance_b(1)
+                continue
+            
+            value = raw_value
+            if token_type == TokenType.ID and len(raw_value) > 80:
+                self._warn(f'identifier "{raw_value}" will be truncated to 80 characters.', len(raw_value))
+                value = raw_value[:80]
+            elif token_type == TokenType.INT_LIT and int(raw_value) >= 2**31:
+                self._warn(f'integer "{raw_value}" will be clamped to 2^31-1.', len(raw_value))
+                value = str(2**31 - 1)
+            
+            token = Token(token_type, value, self._b, self._b+len(raw_value), self._line, self._column)
+            self._advance_b(len(raw_value))
+            
+            if token_type == TokenType.COMMENT and not self._emit_comments:
+                continue
+            else:
                 return token
-            
-            self._warn(f'unrecognized character "{self._s[self._b]}".')
-            self._advance_b(1)
     
     def peek(self):
+        """Returns the next token, as in Scanner.next(), but without consuming it."""
+        # Scan the next token, but then roll back the current position.
         b, line, column = self._b, self._line, self._column
         next = self.next()
         self._b, self._line, self._column = b, line, column
@@ -145,13 +162,9 @@ class Scanner:
         return self
     
     def __next__(self):
+        """Allows for iteration over all the tokens in the source, as if by next(), except that an EOF token is not included."""
         n = self.next()
         if n.type == TokenType.EOF:
             raise StopIteration()
         else:
             return n
-
-# To do: file opening
-# To do: error combination
-# To do: test suites
-# To do: documentation
