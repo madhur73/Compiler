c84faa1fbc7bfd50e6691a39d56add23dee597ed
Max Marrone <max@marrone.nyc>
2019-04-09 15:16:14 -0400

    Reorganize tree.
    Remove the alternative implementation of scanner.
    Remove scanner demo and its support files.
    Move the main source into the root directory, out of assignment1.
    Remove the assignment1 directory.

--- assignment1/demo_stdin.py
+++
@@ -1,6 +0,0 @@
-from scanner import Scanner
-from sys import stdin
-
-s = Scanner(file=stdin)
-for token in s:
-	print(token)
--- token.py
+++ token.py
@@ -1,28 +1,66 @@
-class token:
-	def __init__(self,id,typenm,linenum,startpos,endpos):
-		self.id = id
-		self.type = typenm
-		self.linenum = linenum
-		self.startpos =startpos
-		self.endpos = endpos
-		self.errormsg = ""
-		self.colstart = 0
+import enum
 
-	def setlincol(self,linecol):
-		temp = (linecol[self.linenum]-self.endpos)
-		if(temp<0):
-			self.colend = (self.linenum,temp*(-1))
-		else:
-			self.colend = (self.linenum,temp*(-1))
-		print(self.colend)
+TokenType = enum.Enum("TokenType", """
+    KW_ARRAY
+    ID
+    OP_DOTDOT
+    LBRAK
+    RBRAK
+    SEMI
+    KW_TUPLE
+    KW_LOCAL
+    KW_GLOBAL
+    KW_DEFUN
+    LPAR
+    RPAR
+    OP_COMMA
+    KW_END
+    KW_WHILE
+    KW_DO
+    KW_IF
+    KW_THEN
+    KW_ELSIF
+    KW_ELSE
+    KW_FOREACH
+    KW_IN
+    OP_DOT
+    INT_LIT
+    RETURN
+    PRINT
+    ASSIGN
+    EXCHANGE
+    OP_LESS
+    OP_GREATER
+    OP_LESSEQUAL
+    OP_GREATEREQUAL
+    OP_EQUAL
+    OP_NOTEQUA
+    OP_PLUS
+    OP_MINUS
+    OP_MULT
+    OP_DIV
+    COMMENT
+    EOF
+""") # Note no OP_UMINUS.
+
+class Token:
+    """Represents a token.
     
-	def printall(self):	
-		print(self.id+"\t"+str(self.type)+"\t"+str(self.linenum)+"\t"+str(self.startpos)+"\t"+str(self.endpos)+"\t"+self.errormsg+"\t")
-
-	def seterror(self,msg):
-		self.errormsg = msg
+    - type is a TokenType.
+    - string is the value of the token, which may be modified (e.g. truncated) from
+      exactly how it appeared in the source.
+    - [begin, end) represents the character indices in the source file for where the
+      token appeared, before any truncation or other post-processing.
+    - line and column are the human-readable (1-based) coordinates of begin.
+    """
     
-	def geterrormsg(self):
-		print(self.errormsg+"on line:",self.linenum,"on col start:",self.colstart, "col end:")
-
-
+    def __init__(self, type, string, begin, end, line, column):
+        self.type = type
+        self.string = string
+        self.begin = begin
+        self.end = end
+        self.line = line
+        self.column = column
+    
+    def __str__(self):
+        return '{}:{} ({}-{}) {} "{}"'.format(self.line, self.column, self.begin, self.end, self.type, self.string)
