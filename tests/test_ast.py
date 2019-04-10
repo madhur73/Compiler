b826781160cb59fedafdf845ff46dcdf662c5782
Max Marrone <max@marrone.nyc>
2019-04-10 18:10:19 -0400

    Create test_ast.py with a skeleton, to be done later.

---
+++ tests/test_ast.py
@@ -0,0 +1,16 @@
+from bmc.ast import *
+
+def test_equal():
+	pass
+	
+def test_equal_despite_different_token_locations():
+	pass
+
+def test_unequal_different_structures():
+	pass
+
+def test_unequal_different_types():
+	pass
+
+def test_unequal_different_token_values():
+	pass
