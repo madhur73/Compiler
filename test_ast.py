83dc6e7c0b4e9f94ef34b5294f3671a05613fb49
codingisbliss <42849460+codingisbliss@users.noreply.github.com>
2019-04-12 23:06:08 -0400

    Add files via upload

---
+++ test_ast.py
@@ -0,0 +1,19 @@
+from bmc.ast import *
+from bmc import scanner
+from bmc.token import TokenType
+from bmc.parse import *
+
+print("Welcome to...\n")
+print("-------\n|B|M|C\n--------\nversion1.0.0")
+
+for i in range(7):
+	
+	print("-------------------------test case "+str(i)+" -------------------------")
+	file = open("tests/p2test"+str(i)+".txt","r") 
+	print(file.read())
+	print("------------------------- AST ---------------------------------")
+
+	s = scanner.Scanner(emit_comments = False,filepath="tests/p2test"+str(i)+".txt")
+	p = parse_declaration(s)
+	print(p)		
+		
