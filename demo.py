31d52fa97d5fe0dcef6b0581edc8365f7370792e
Max Marrone <max@marrone.nyc>
2019-04-12 23:38:04 -0400

    Make demo.py use the same test function that test_ast does.

--- demo.py
+++ demo.py
@@ -3,11 +3,4 @@ import bmc.parse
 from sys import stdin
 print("Reading from standard input...")
 scanner = bmc.scanner.Scanner(file=stdin, emit_comments=False)
-try:
-	ast = bmc.parse.parse_program(scanner)
-	print("Success!")
-	print(ast)
-except bmc.parse.ParseError as error:
-	print("Error.")
-	print("Got", scanner.peek())
-	print("But expected " + " or ".join(str(t) for t in error.expected))
+bmc.parse.parse_report_errors(scanner)
