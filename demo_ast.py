128e428e4ac965e6700064dcdbd13619a6f4c5bf
Max Marrone <max@marrone.nyc>
2019-05-15 15:27:44 -0400

    Separate demo files.

--- demo_ast.py
+++ demo_ast.py
@@ -4,5 +4,6 @@ from sys import stdin
 print("Reading from standard input...")
 scanner = bmc.scanner.Scanner(file=stdin, emit_comments=False)
 ast = bmc.parse.parse_report_errors(scanner)
+print(ast)
 ast.type_check()
 print("Done type check")
