2d110675ca7c18ec83be9394a025aa0cdf53b34b
Max Marrone <max@marrone.nyc>
2019-05-15 04:24:27 -0400

    Convert ast.py and test_ast.py indentation to spaces.

--- bmc/ast.py
+++ bmc/ast.py
@@ -2,46 +2,46 @@ from bmc.token import Token
 from typing import List, Union, Optional, get_type_hints
 
 class Node:
-	"""Base class for all abstract syntax tree nodes."""
-	
-	def __repr__(self):
-		def indent(string):
-			return "\n".join("  " + line for line in string.split("\n"))
-		def pretty(value):
-			# For non-empty lists, start a new level of indentation and put each element on its own line.
-			if isinstance(value, list) and len(value) > 0:
-				return "[\n" + indent(",\n".join(repr(e) for e in value)) + "\n]"
-			else:
-				return repr(value)
-		child_strings = [indent(name + " = " + pretty(value)) for name, value in self.children()]
-		return type(self).__name__ + "(\n" + ",\n".join(child_strings) + "\n)"
-	
-	def children(self):
-		return [i for i in vars(self).items()]
-	
-	# Auto-generates the node class's __init__().
-	def __init_subclass__(subclass):
-		child_names = _type_annotation_names(subclass)
-		def __init__(self, *, location_begin=None, location_end=None, **kwargs):
-			self.location_begin = location_begin
-			self.location_end = location_end
-			for name in child_names:
-				setattr(self, name, kwargs.pop(name))
-			if kwargs:
-				raise TypeError("Unrecognized arguments in Node initialization: " + str(kwargs))
-		subclass.__init__ = __init__
-	
-	def __eq__(self, other):
-		if type(self) != type(other):
-			return NotImplemented
-		return self.children() == other.children()
-		
+    """Base class for all abstract syntax tree nodes."""
+    
+    def __repr__(self):
+        def indent(string):
+            return "\n".join("  " + line for line in string.split("\n"))
+        def pretty(value):
+            # For non-empty lists, start a new level of indentation and put each element on its own line.
+            if isinstance(value, list) and len(value) > 0:
+                return "[\n" + indent(",\n".join(repr(e) for e in value)) + "\n]"
+            else:
+                return repr(value)
+        child_strings = [indent(name + " = " + pretty(value)) for name, value in self.children()]
+        return type(self).__name__ + "(\n" + ",\n".join(child_strings) + "\n)"
+    
+    def children(self):
+        return [i for i in vars(self).items()]
+    
+    # Auto-generates the node class's __init__().
+    def __init_subclass__(subclass):
+        child_names = _type_annotation_names(subclass)
+        def __init__(self, *, location_begin=None, location_end=None, **kwargs):
+            self.location_begin = location_begin
+            self.location_end = location_end
+            for name in child_names:
+                setattr(self, name, kwargs.pop(name))
+            if kwargs:
+                raise TypeError("Unrecognized arguments in Node initialization: " + str(kwargs))
+        subclass.__init__ = __init__
+    
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        return self.children() == other.children()
+        
 
 def _type_annotation_names(cls):
-	annotations = dict()
-	for c in reversed(cls.mro()):
-		annotations.update(getattr(c, "__annotations__", {}))
-	return annotations.keys()
+    annotations = dict()
+    for c in reversed(cls.mro()):
+        annotations.update(getattr(c, "__annotations__", {}))
+    return annotations.keys()
  
 # Descendents of Node are given an auto-generated __init__().  The node's children
 # are passed in as keyword-only arguments; the names of these arguments are
@@ -53,96 +53,96 @@ def _type_annotation_names(cls):
 # Root node:
 
 class Program(Node):
-	parts: List[Union["Statement", "FunctionDefinition", "Declaration"]]
+    parts: List[Union["Statement", "FunctionDefinition", "Declaration"]]
 
 
 # Declarations:
 
 class Declaration(Node): # Abstract base class.
-	pass
+    pass
 
 # array a[0..9] = i=i*2
 class ArrayDeclaration(Declaration):
-	identifier: Token
-	range: "Range"
-	index_identifier: Optional[Token]
-	index_expression: Optional["Expression"]
+    identifier: Token
+    range: "Range"
+    index_identifier: Optional[Token]
+    index_expression: Optional["Expression"]
 
 # local x = 1
 class LocalDeclaration(Declaration):
-	identifier: Token
-	expression: Optional["Expression"]
+    identifier: Token
+    expression: Optional["Expression"]
 
 # global x = 1
 class GlobalDeclaration(Declaration):
-	identifier: Token
-	expression: Optional["Expression"]
+    identifier: Token
+    expression: Optional["Expression"]
 
 # Definitions:
 
 # defun f(a, b, c) ... end defun
 class FunctionDefinition(Node):
-	function_identifier: Token
-	argument_identifiers: List[Token]
-	body: List[Union["Declaration", "Statement"]]
+    function_identifier: Token
+    argument_identifiers: List[Token]
+    body: List[Union["Declaration", "Statement"]]
 
 
 # Statements:
 
 class Statement(Node): # Abstract base class.
-	pass
+    pass
 
 # a, t.1, a[1]
 class LHS(Node):
-	elements: List[Union["IdentifierExpression", "TupleAccessExpression", "ArrayAccessExpression"]]
+    elements: List[Union["IdentifierExpression", "TupleAccessExpression", "ArrayAccessExpression"]]
 
 # a = b
 class AssignmentStatement(Statement):
-	left: "LHS"
-	right: "Expression"
+    left: "LHS"
+    right: "Expression"
 
 # a <-> b
 class ExchangeStatement(Statement):
-	left: "LHS"
-	right: "LHS"
+    left: "LHS"
+    right: "LHS"
 
 # while a == b do ... end while
 class WhileStatement(Statement):
-	condition: "BooleanExpression"
-	body: List["Statement"]
+    condition: "BooleanExpression"
+    body: List["Statement"]
 
 # if a == b then ... end if
 class IfStatement(Statement):
-	condition: "BooleanExpression"
-	body: List["Statement"]
-	else_body: List["Statement"]
+    condition: "BooleanExpression"
+    body: List["Statement"]
+    else_body: List["Statement"]
 
 # foreach i in s do ... end for
 class ForeachStatement(Statement):
-	element_identifier: Token
-	source_sequence: Union["IdentifierExpression", "Range"]
-	body: List["Statement"]
+    element_identifier: Token
+    source_sequence: Union["IdentifierExpression", "Range"]
+    body: List["Statement"]
 
 # return x
 class ReturnStatement(Statement):
-	expression: "Expression"
+    expression: "Expression"
 
 # print x
 class PrintStatement(Statement):
-	expression: "Expression"
+    expression: "Expression"
 
 
 # Expressions:
 
 # a..b
 class Range(Node):
-	begin_expression: "Expression"
-	end_expression: "Expression"
+    begin_expression: "Expression"
+    end_expression: "Expression"
 
 # a < b, a == b, etc.
 class BooleanExpression(Node):
-	left: "Expression"
-	right: "Expression"
+    left: "Expression"
+    right: "Expression"
 class LessThanExpression(BooleanExpression): pass
 class LessThanEqualsExpression(BooleanExpression): pass
 class GreaterThanExpression(BooleanExpression): pass
@@ -151,16 +151,16 @@ class EqualsExpression(BooleanExpression): pass
 class NotEqualsExpression(BooleanExpression): pass
 
 class Expression(Node): # Abstract base class.
-	pass
+    pass
 
 # a, b, c
 class TupleExpression(Expression):
-	elements: List["Expression"]
+    elements: List["Expression"]
 
 # a+b, a-b, a*b, a/b
 class ArithmeticExpression(Expression):
-	left: "Expression"
-	right: "Expression"
+    left: "Expression"
+    right: "Expression"
 class AddExpression(ArithmeticExpression): pass
 class SubtractExpression(ArithmeticExpression): pass
 class MultiplyExpression(ArithmeticExpression): pass
@@ -168,23 +168,23 @@ class DivideExpression(ArithmeticExpression): pass
 
 # a
 class IdentifierExpression(Expression):
-	token: Token
+    token: Token
 
 # f x
 class FunctionCallExpression(Expression):
-	identifier_token: Token
-	argument: "Expression"
+    identifier_token: Token
+    argument: "Expression"
 
 # t.1
 class TupleAccessExpression(Expression):
-	identifier_token: Token
-	index: Token
+    identifier_token: Token
+    index: Token
 
 # a[i]
 class ArrayAccessExpression(Expression):
-	identifier_token: Token
-	index: "Expression"
+    identifier_token: Token
+    index: "Expression"
 
 # 123
 class IntegerLiteralExpression(Expression):
-	token: Token
+    token: Token
--- tests/test_ast.py
+++ tests/test_ast.py
@@ -2,37 +2,37 @@ import pytest
 from bmc import ast
 
 def test_init_requires_named_arguments():
-	with pytest.raises(TypeError):
-		n = ast.EqualsExpression("a", "b")
-	n = ast.EqualsExpression(left="a", right="b")
-	assert (n.left, n.right) == ("a", "b")
+    with pytest.raises(TypeError):
+        n = ast.EqualsExpression("a", "b")
+    n = ast.EqualsExpression(left="a", right="b")
+    assert (n.left, n.right) == ("a", "b")
 
 def test_init_rejects_extra_arguments():
-	with pytest.raises(TypeError):
-		n = ast.EqualsExpression(left="a", right="b", extra="c")
+    with pytest.raises(TypeError):
+        n = ast.EqualsExpression(left="a", right="b", extra="c")
 
 def test_init_accepts_optional_location():
-	n = ast.EqualsExpression(left="a", right="b", location_begin=0, location_end=1)
-	assert (n.location_begin, n.location_end) == (0, 1)
+    n = ast.EqualsExpression(left="a", right="b", location_begin=0, location_end=1)
+    assert (n.location_begin, n.location_end) == (0, 1)
 
 def test_equality():
-	x_lt_y_1 = ast.LessThanExpression(left="x", right="y")
-	x_lt_y_2 = ast.LessThanExpression(left="x", right="y")
-	x_gt_y = ast.GreaterThanExpression(left="x", right="y")
-	y_gt_x = ast.GreaterThanExpression(left="y", right="x")
-	assert x_lt_y_1 == x_lt_y_2 # Equal: same type, same contents.
-	assert x_lt_y_1 != x_gt_y # Not equal: same structure but different type.
-	assert x_gt_y != y_gt_x # Not equal: same type but different contents.
-	
+    x_lt_y_1 = ast.LessThanExpression(left="x", right="y")
+    x_lt_y_2 = ast.LessThanExpression(left="x", right="y")
+    x_gt_y = ast.GreaterThanExpression(left="x", right="y")
+    y_gt_x = ast.GreaterThanExpression(left="y", right="x")
+    assert x_lt_y_1 == x_lt_y_2 # Equal: same type, same contents.
+    assert x_lt_y_1 != x_gt_y # Not equal: same structure but different type.
+    assert x_gt_y != y_gt_x # Not equal: same type but different contents.
+    
 def test_repr():
-	n = ast.IfStatement(
-	    condition=ast.LessThanExpression(
-	        left=ast.IdentifierExpression(token="i"),
-	        right=ast.IntegerLiteralExpression(token="123")
-	    ),
-	    body=[ast.PrintStatement(expression=ast.IdentifierExpression(token="x"))],
-	    else_body=[]
-	)
-	namespace = {}
-	exec("from bmc.ast import *; m = " + repr(n), globals(), namespace)
-	assert n == namespace["m"]
+    n = ast.IfStatement(
+        condition=ast.LessThanExpression(
+            left=ast.IdentifierExpression(token="i"),
+            right=ast.IntegerLiteralExpression(token="123")
+        ),
+        body=[ast.PrintStatement(expression=ast.IdentifierExpression(token="x"))],
+        else_body=[]
+    )
+    namespace = {}
+    exec("from bmc.ast import *; m = " + repr(n), globals(), namespace)
+    assert n == namespace["m"]
