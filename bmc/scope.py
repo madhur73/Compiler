52a2c87de8843a1630d3aee10d748abbaccbd637
Max Marrone <max@marrone.nyc>
2019-05-19 07:33:03 -0400

    Add scope.py and some tests.

--- bmc/scope.py
+++ bmc/scope.py
@@ -1,6 +1,10 @@
 """Classes for keeping track of all the variables in the current scope, as well as their types."""
 
 from collections import namedtuple
+from bmc.errors import ReportableError
+
+class ScopeError(ReportableError):
+    pass
 
 class TupleType:
     """Represents the type of a tuple variable.  Integers are modeled as 1-tuples."""
@@ -12,9 +16,6 @@ class TupleType:
     def __eq__(self, other):
         return isinstance(other, TupleType) and self.length == other.length
 
-# TODO: Figure out a nice way to expose the most recent values for each element
-# of a tuple.
-
 class ArrayType:
     """Represents the type of an array variable.  Does not have to be instantiated; ArrayType is equivalent to ArrayType()."""
     def __new__(cls):
@@ -24,13 +25,67 @@ class ArrayType:
         return "ArrayType()"
 
 class Scope:
+    """Represents the set of variables available for use at a certain point in the program.
+    
+    For convenience, variables are looked up by passing in tokens whose values
+    represent their identifier.
+    
+    Scopes can be nested.  Name lookups in the inner scopes will fall back to the outer scopes.
+    """
     def __init__(self, parent=None):
         self.parent = parent
         self.namespace = {}
-    def is_global():
+    def is_global(self):
+        """Returns whether this is the global scope."""
         return self.parent == None
     def add_global_declaration(self, token, type, slot):
-        self.namespace[token.string] = token, type, slot
+        """Add a global declaration.
+        
+        This corresponds both to creating new global variables in the global
+        scope, and to declaring global references in function scopes.
+        
+        If type is None, slot should also be None.  If this is the global scope,
+        An array declaration is expected to follow.
+        
+        If type is a TupleType, slot should be an LLVM pointer value that points
+        to the LLVM array containing the tuple elements.  The scope must be
+        global, in this case.
+        """
+        
+        if not self.is_global():
+            raise ScopeError("Non-global scopes are not yet implemented.", token) 
+        elif token.string in self.namespace:
+            raise ScopeError(f"Global variable {token.string} already declared.", token)
+        else:
+            self.namespace[token.string] = token, type, slot
+    
+    def add_array_declaration(self, token, slot):
+        """Add an array declaration.
+        
+        The token must have been previously associated with a global or local
+        declaration in this scope with an empty type and slot.
+        """
+        # Ensure the variable is present in the current scope, even though
+        # we're about to overwrite its type and slot.
+        _, _ = self.current_scope_lookup(token)
+        self.namespace[token.string] = token, ArrayType, slot
+    
     def lookup(self, token):
-        token, type, slot = self.namespace[token.string]
-        return type, slot
+        try:
+            return self.current_scope_lookup(token)
+        except ScopeError:
+            if self.parent:
+                return self.parent.lookup(token)
+            else:
+                raise ScopeError(f"Variable {token.string} is undeclared.", token)
+    
+    def current_scope_lookup(self, token):
+        """Finds a variable in the current scope."""
+        try:
+            token, type, slot = self.namespace[token.string]
+            return type, slot
+        except KeyError:
+            return ScopeError(f"Variable {token.string} is undeclared.", token)
+    
+    def global_lookup(self, token):
+        pass
--- tests/test_scope.py
+++ tests/test_scope.py
@@ -1,12 +1,29 @@
-from bmc import scope
+import pytest
+from bmc import scope, token
 
-def test_tuple_type():
-    assert scope.TupleType(1) != scope.TupleType(2)
-    assert scope.TupleType(1) == scope.TupleType(1)
+def new_token(identifier):
+    return token.Token(token.TokenType.ID, identifier, 0, 0, 0, 0)
 
-def test_array_type():
-    assert scope.ArrayType is scope.ArrayType()
-    assert scope.ArrayType() is scope.ArrayType()
+def test_global_or_local():
+    a = scope.Scope()
+    b = scope.Scope(a)
+    c = scope.Scope(b)
+    assert a.is_global()
+    assert not b.is_global()
+    assert not c.is_global()
 
-def test_different_types():
-    assert scope.ArrayType != scope.TupleType(1)
+def test_global_tuple_lookup():
+    s = scope.Scope()
+    s.add_global_declaration(new_token("x"), scope.TupleType(1), "x_slot")
+    s.add_global_declaration(new_token("y"), scope.TupleType(2), "y_slot")
+    assert s.lookup(new_token("x")) == (scope.TupleType(1), "x_slot")
+    assert s.lookup(new_token("y")) == (scope.TupleType(2), "y_slot")
+
+@pytest.mark.xfail
+def test_shadowing():
+    outer = scope.Scope()
+    outer.add_global_declaration(new_token("x"), scope.TupleType(1), None)
+    inner = scope.Scope(outer)
+    inner.add_global_declaration(new_token("x"), scope.TupleType(2), None)
+    assert outer.lookup(new_token("x")) == (scope.TupleType(1), None)
+    assert inner.lookup(new_token("x")) == (scope.TupleType(2), None)
