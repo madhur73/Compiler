17a599baf11cd7b4c8331cbca4b995cf12f94e51
Max Marrone <max@marrone.nyc>
2019-05-19 20:48:57 -0400

    Remove demo_ast.py.

--- demo_ast.py
+++
@@ -1,9 +0,0 @@
-import bmc.scanner
-import bmc.parse
-from sys import stdin
-print("Reading from standard input...")
-scanner = bmc.scanner.Scanner(file=stdin, emit_comments=False)
-ast = bmc.parse.parse_report_errors(scanner)
-print(ast)
-ast.type_check()
-print("Done type check")
