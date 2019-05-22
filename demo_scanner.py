1ec636673417a18e216ac3518809f99afa116d2c
Max Marrone <max@marrone.nyc>
2019-05-22 05:08:20 -0400

    Remove demo_scanner.py.

--- demo_scanner.py
+++
@@ -1,7 +0,0 @@
-import bmc.scanner
-import bmc.parse
-from sys import stdin
-print("Reading from standard input...")
-scanner = bmc.scanner.Scanner(file=stdin, emit_comments=False)
-for token in scanner:
-    print(token)
